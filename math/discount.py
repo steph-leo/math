a26ae74368da0f846c4cef2fe83bfd06106929d8
Stephano Siame <141557641+steph-leo@users.noreply.github.com>
2024-12-17 01:50:22 +0300

    Update discount.py

--- math/discount.py
+++ math/discount.py
@@ -5,6 +5,6 @@ if purchase_amount <= 1000:
 elif purchase_amount <= 500:
     discount = 0.05 #discount 5%
 else:
-    discount = 0
+    discount = 0 #no discount
 final_price = purchase_amount * (1 - discount)
 print ("price is :", final_price)
