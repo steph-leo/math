ba5dd81c3faa1a0828a1b2b0148c5b6eefd57e5d
steph-leo <leostephs89@gmail.com>
2024-12-17 01:48:55 +0300

    file discount.py in

---
+++ math/discount.py
@@ -0,0 +1,10 @@
+purchase_amount = float(input("Enter the amount payed"))
+
+if purchase_amount <= 1000:
+    discount = 0.1 #discount is 10%
+elif purchase_amount <= 500:
+    discount = 0.05 #discount 5%
+else:
+    discount = 0
+final_price = purchase_amount * (1 - discount)
+print ("price is :", final_price)
